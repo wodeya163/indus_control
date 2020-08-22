391f414fcbb8d0311d5e59114113d6f82ac47261
wodeya163 <43662257+wodeya163@users.noreply.github.com>
2020-08-22 19:57:39 +0800

    添加项目文件。

---
+++ ModbusDAL/Modbus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ModbusDAL
+{
+    public class Modbus
+    {
+        private SerialPort MySeialPort;
+        private int BandRate;
+        private string PortNumber;
+        private int DataBits;
+        private Parity ParityBit;
+        private StopBits StopBit;
+
+        public Modbus()
+        {
+            MySeialPort = new SerialPort();
+        }
+         
+
+    }
+
+}
